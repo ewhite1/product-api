b0e82478bd239abba02d7fa47f0d71e5d5ebef1d
Edward White <edwardwhite1@outlook.com>
2020-03-10 18:51:34 -0500

    Created a better JSON encoding method using io writer instead of the json marshaler

--- data/products.go
+++ data/products.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 // Product define the structure for the API, with a Staticly defined data before we introduce a db
 type Product struct {
@@ -14,7 +18,14 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
-func GetProducts() []*Product {
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
 	return productList
 }
 
